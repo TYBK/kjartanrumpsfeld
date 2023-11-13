21f89f0b00251751e7de48f063d5a059b4411c9d
TYBK <135532606+TYBK@users.noreply.github.com>
2023-11-13 07:32:29 -0700

    Add files via upload

---
+++ kjartan rumpsfeld/config.js
@@ -0,0 +1,11 @@
+ // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCRilGGGODbZLcbD4YXgLvGYf7xOkiyciA",
+    authDomain: "jxandco-a78f8.firebaseapp.com",
+    projectId: "jxandco-a78f8",
+    storageBucket: "jxandco-a78f8.appspot.com",
+    messagingSenderId: "524654421284",
+    appId: "1:524654421284:web:a19c2bed761738f6febfb1",
+    measurementId: "G-CBL0R583RS"
+  };
